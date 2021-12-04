df800f862fdc62db3359a16392891ebb9fdb21ef
Albie <49698766+albie6544@users.noreply.github.com>
2021-12-04 16:20:22 -0500

    Polish

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-$ipv6 = false;
-
 function os() : string {
 
 	$user_agent = $_SERVER['HTTP_USER_AGENT'];
@@ -9,29 +7,29 @@ function os() : string {
 	$platform = "unknown";
 	
 	$os_array = array(
-		'/windows nt 10/i'      =>  'Windows 10',
-		'/windows nt 6.3/i'     =>  'Windows 8.1',
-		'/windows nt 6.2/i'     =>  'Windows 8',
-		'/windows nt 6.1/i'     =>  'Windows 7',
-		'/windows nt 6.0/i'     =>  'Windows Vista',
-		'/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
-		'/windows nt 5.1/i'     =>  'Windows XP',
-		'/windows xp/i'         =>  'Windows XP',
-		'/windows nt 5.0/i'     =>  'Windows 2000',
-		'/windows me/i'         =>  'Windows ME',
-		'/win98/i'              =>  'Windows 98',
-		'/win95/i'              =>  'Windows 95',
-		'/win16/i'              =>  'Windows 3.11',
-		'/macintosh|mac os x/i' =>  'Mac OS X',
-		'/mac_powerpc/i'        =>  'Mac OS 9',
-		'/linux/i'              =>  'Linux',
-		'/ubuntu/i'             =>  'Ubuntu',
-		'/iphone/i'             =>  'iPhone',
-		'/ipod/i'               =>  'iPod',
-		'/ipad/i'               =>  'iPad',
-		'/android/i'            =>  'Android',
-		'/blackberry/i'         =>  'BlackBerry',
-		'/webos/i'              =>  'WebOS'
+		'/windows nt 10/i'      =>  'win-10',
+		'/windows nt 6.3/i'     =>  'win-8.1',
+		'/windows nt 6.2/i'     =>  'win-8',
+		'/windows nt 6.1/i'     =>  'win-7',
+		'/windows nt 6.0/i'     =>  'win-vista',
+		'/windows nt 5.2/i'     =>  'win-2003',
+		'/windows nt 5.1/i'     =>  'win-xp',
+		'/windows xp/i'         =>  'win-xp',
+		'/windows nt 5.0/i'     =>  'win-2000',
+		'/windows me/i'         =>  'win-me',
+		'/win98/i'              =>  'win-98',
+		'/win95/i'              =>  'win-95',
+		'/win16/i'              =>  'win-3',
+		'/macintosh|mac os x/i' =>  'macos-x',
+		'/mac_powerpc/i'        =>  'macos-9',
+		'/linux/i'              =>  'linux',
+		'/ubuntu/i'             =>  'ubuntu',
+		'/iphone/i'             =>  'iphone',
+		'/ipod/i'               =>  'ipod',
+		'/ipad/i'               =>  'ipad',
+		'/android/i'            =>  'android',
+		'/blackberry/i'         =>  'blackberry',
+		'/webos/i'              =>  'webos'
 	);
 
 	foreach ( $os_array as $regex => $value ) { 
@@ -48,16 +46,16 @@ function br() : string {
 	$browser = "unknown";
 	
 	$browser_array = array(
-		'/msie/i'       =>  'Internet Explorer',
-		'/firefox/i'    =>  'Firefox',
-		'/safari/i'     =>  'Safari',
-		'/chrome/i'     =>  'Chrome',
-		'/edge/i'       =>  'Edge',
-		'/opera/i'      =>  'Opera',
-		'/netscape/i'   =>  'Netscape',
-		'/maxthon/i'    =>  'Maxthon',
-		'/konqueror/i'  =>  'Konqueror',
-		'/mobile/i'     =>  'Mobile'
+		'/msie/i'       =>  'ie',
+		'/firefox/i'    =>  'firefox',
+		'/safari/i'     =>  'safari',
+		'/chrome/i'     =>  'chrome',
+		'/edge/i'       =>  'edge',
+		'/opera/i'      =>  'opera',
+		'/netscape/i'   =>  'netscape',
+		'/maxthon/i'    =>  'maxthon',
+		'/konqueror/i'  =>  'konqueror',
+		'/mobile/i'     =>  'mobile'
 	);
 
 	foreach ( $browser_array as $regex => $value ) { 
@@ -89,34 +87,38 @@ function dec($ip, $ipv6) : string {
     if (!$ipv6) {
         $nums = explode('.', $ip);
 
-        return strval(($nums[0] * 16777216) + ($nums[1] * 65536) + ($nums[2] * 256) + ($nums[3] * 1));
+        $dec = ($nums[0] * 16777216) + ($nums[1] * 65536) + ($nums[2] * 256) + ($nums[3] * 1);
+
+        return strval($dec);
     } else {
-        $str = '';
+        $dec = '';
+
         foreach (unpack('C*', inet_pton($ip)) as $byte) {
-            $str .= str_pad(decbin($byte), 8, '0', STR_PAD_LEFT);
-        }
-        $str = ltrim($str, '0');
-        if (function_exists('bcadd')) {
-            $numeric = 0;
-            for ($i = 0; $i < strlen($str); $i++) {
-                $right  = base_convert($str[$i], 2, 10);
-                $numeric = bcadd(bcmul($numeric, 2), $right);
-            }
-            $str = $numeric;
-        } else {
-            $str = base_convert($str, 2, 10);
+            $dec .= str_pad(decbin($byte), 8, '0', STR_PAD_LEFT);
         }
 
-        return $str;
+        $dec = ltrim($dec, '0');
+
+        if (function_exists('bcadd')) {
+            $num = 0;
+
+            for ($i = 0; $i < strlen($dec); $i++) {
+                $right  = base_convert($dec[$i], 2, 10);
+                $num = bcadd(bcmul($num, 2), $right);
+            }
+
+            $dec = $num;
+        } else {
+            $dec = base_convert($dec, 2, 10);
+        }
+
+        return $dec;
     }
 }
 
-echo '{"address":"' . ip() . '","decimal":"' . dec(ip(), $ipv6) . '","system":"' . os() . '","browser":"' . br() . '"}';
-echo '{';
-echo '  "version":"{}",';
-echo '  "address":"{ip()}",';
-echo '  "decimal":"{dec(ip(), $ipv6)}",';
-echo '  "system":"{os()}",';
-echo '  "browser":"{br()}",';
-echo '  "":"{}",';
-echo '}';
+function v6($ip) {
+    
+    return false;
+}
+
+echo '{ "version":"' . v6(ip()) . '", "address":"'. ip() . '", "decimal":"'. dec(ip(), v6(ip())) . '", "system":"'. os() . '", "browser":"'. br() . '" }';
