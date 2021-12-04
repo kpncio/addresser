7df02faea6a12ed501ca54826305731a670b2fc1
Albie <49698766+albie6544@users.noreply.github.com>
2021-12-04 15:47:00 -0500

    Old Init

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
-function getos() : string {
+$ipv6 = false;
+
+function os() : string {
 
 	$user_agent = $_SERVER['HTTP_USER_AGENT'];
 
-	$os_platform =   "UNKNOWN";
+	$platform = "unknown";
 	
-	$os_array =   array(
+	$os_array = array(
 		'/windows nt 10/i'      =>  'Windows 10',
 		'/windows nt 6.3/i'     =>  'Windows 8.1',
 		'/windows nt 6.2/i'     =>  'Windows 8',
@@ -34,18 +36,18 @@ function getos() : string {
 
 	foreach ( $os_array as $regex => $value ) { 
 		if ( preg_match($regex, $user_agent ) ) {
-			$os_platform = $value;
+			$platform = $value;
 		}
 	}   
-	return $os_platform;
+	return $platform;
 }
 
-function getbr() : string {
+function br() : string {
 	$user_agent = $_SERVER['HTTP_USER_AGENT'];
 
-	$browser        = "UNKNOWN";
+	$browser = "unknown";
 	
-	$browser_array  = array(
+	$browser_array = array(
 		'/msie/i'       =>  'Internet Explorer',
 		'/firefox/i'    =>  'Firefox',
 		'/safari/i'     =>  'Safari',
@@ -67,7 +69,7 @@ function getbr() : string {
 	return $browser;
 }
 
-function getip() : string {
+function ip() : string {
 	$keys = array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR');
 
 	foreach($keys as $k)
@@ -80,21 +82,41 @@ function getip() : string {
 		}
 	}
 
-	return "UNKNOWN";
+	return "unknown";
 }
 
-function getdc($ip) : string {
-    return inet_ntop($ip);
-}
+function dec($ip, $ipv6) : string {
+    if (!$ipv6) {
+        $nums = explode('.', $ip);
 
-function getvn() : bool {
-    $ip = getip();
-
-    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-        return false;
+        return strval(($nums[0] * 16777216) + ($nums[1] * 65536) + ($nums[2] * 256) + ($nums[3] * 1));
     } else {
-        return true;
+        $str = '';
+        foreach (unpack('C*', inet_pton($ip)) as $byte) {
+            $str .= str_pad(decbin($byte), 8, '0', STR_PAD_LEFT);
+        }
+        $str = ltrim($str, '0');
+        if (function_exists('bcadd')) {
+            $numeric = 0;
+            for ($i = 0; $i < strlen($str); $i++) {
+                $right  = base_convert($str[$i], 2, 10);
+                $numeric = bcadd(bcmul($numeric, 2), $right);
+            }
+            $str = $numeric;
+        } else {
+            $str = base_convert($str, 2, 10);
+        }
+
+        return $str;
     }
 }
 
-echo '{"ipv6":"' . strval(getvn()) . '","address":"' . getip() . '","decimal":"' . getdc(getip()) . '","system":"' . getos() . '","browser":"' . getbr() . '"}';
+echo '{"address":"' . ip() . '","decimal":"' . dec(ip(), $ipv6) . '","system":"' . os() . '","browser":"' . br() . '"}';
+echo '{';
+echo '  "version":"{}",';
+echo '  "address":"{ip()}",';
+echo '  "decimal":"{dec(ip(), $ipv6)}",';
+echo '  "system":"{os()}",';
+echo '  "browser":"{br()}",';
+echo '  "":"{}",';
+echo '}';
