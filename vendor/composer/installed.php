3e83fb8213f6af0bfaf7010ee02c28604f26db9b
Albie <49698766+albie6544@users.noreply.github.com>
2021-12-04 14:13:01 -0500

    IP2L Module

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5673070a7723f155c00dcebd4557d538c36d019e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5673070a7723f155c00dcebd4557d538c36d019e',
+            'dev_requirement' => false,
+        ),
+        'ip2location/ip2location-php' => array(
+            'pretty_version' => '9.3.0',
+            'version' => '9.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ip2location/ip2location-php',
+            'aliases' => array(),
+            'reference' => 'c9ac763ac47925364803a361819ec8aebe4ca489',
+            'dev_requirement' => false,
+        ),
+    ),
+);
