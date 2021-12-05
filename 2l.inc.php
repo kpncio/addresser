ccfb707cd61cdbfec6af5fb914b6ad866feac76a
Albie <49698766+albie6544@users.noreply.github.com>
2021-12-05 02:55:33 -0500

    IP2Location

--- 2l.inc.php
+++ 2l.inc.php
@@ -1,10 +1,10 @@
 <?php
 
-function zone($con, $ip6, $dec) {
-    return null;
-}
+require 'db.inc.php';
 
-function country($con, $ip6, $dec) {
+function sdb($ip6, $dec, $tbl, $col, $ary) {
+    $con = odb();
+
     $response = "unknown";
 
     if (!$ip6) {
@@ -13,50 +13,68 @@ function country($con, $ip6, $dec) {
         $ver = "_ipv6";
     }
 
-    $query = "SELECT * FROM `ip2location_db11{$ver}` WHERE `ip_from` <= {$dec} AND `ip_to` >= {$dec} ORDER BY `ip_from` DESC;";
+    $query = "SELECT * FROM `ip2location_{$tbl}{$ver}` WHERE `ip_from` <= {$dec} AND `ip_to` >= {$dec} ORDER BY `ip_from` DESC;";
 
     if ($result = $con -> query($query)) {
         $row = $result -> fetch_assoc();
 
-        $response = strval($row[2]);
-        # $response = join(", ", $row);
+        $response = implode(",", $row);
+        $response = explode(",", $response);
+        if (!$ary) {
+            $response = $response[$col];
+        }
 
         $result -> free_result();
     }
 
-    $con -> close();
+    cdb($con);
 
     return $response;
 }
 
-function region($con, $ip6, $dec) {
-    return null;
+function all($ip6, $dec) {
+    $loc = sdb($ip6, $dec, "db11", 0, true);
+    $asn = sdb($ip6, $dec, "asn", 0, true);
+
+    return array($loc, $asn);
 }
 
-function city($con, $ip6, $dec) {
-    return null;
+function zone($ip6, $dec) {
+    return sdb($ip6, $dec, "db11", 9, false);
 }
 
-function zip($con, $ip6, $dec) {
-    return null;
+function country($ip6, $dec) {
+    return sdb($ip6, $dec, "db11", 2, false);
 }
 
-function latitude($con, $ip6, $dec) {
-    return null;
+function region($ip6, $dec) {
+    return sdb($ip6, $dec, "db11", 4, false);
 }
 
-function longitude($con, $ip6, $dec) {
-    return null;
+function city($ip6, $dec) {
+    return sdb($ip6, $dec, "db11", 5, false);
 }
 
-function cidr($con, $ip6, $dec) {
-    return null;
+function zip($ip6, $dec) {
+    return sdb($ip6, $dec, "db11", 8, false);
 }
 
-function asn($con, $ip6, $dec) {
-    return null;
+function latitude($ip6, $dec) {
+    return sdb($ip6, $dec, "db11", 6, false);
 }
 
-function isp($con, $ip6, $dec) {
-    return null;
+function longitude($ip6, $dec) {
+    return sdb($ip6, $dec, "db11", 7, false);
+}
+
+function cidr($ip6, $dec) {
+    return sdb($ip6, $dec, "asn", 2, false);
+}
+
+function asn($ip6, $dec) {
+    return sdb($ip6, $dec, "asn", 3, false);
+}
+
+function isp($ip6, $dec) {
+    return sdb($ip6, $dec, "asn", 4, false);
 }
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 header('Content-Type: text/plain');
 
 require 'ip.inc.php';
-require 'db.inc.php';
 require '2l.inc.php';
 
 $version = v6(ip(), true);
@@ -12,16 +11,7 @@ $decimal = dc(ip(), v6(ip(), false));
 $system = os();
 $browser = br();
 
-$zone = zone(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$country = country(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$region = region(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$city = city(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$zip = zip(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$latitude = latitude(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$longitude = longitude(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$cidr = cidr(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$asn = asn(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
-$isp = isp(odb(), v6(ip(), false), dc(ip(), v6(ip(), false)));
+$ip2l = all(v6(ip(), false), dc(ip(), v6(ip(), false)));
 
 echo "{ \n";
 # echo "\t \t \"\": \"{$}\" \n";
@@ -33,15 +23,15 @@ echo "\t \t \"system\": \"{$system}\", \n";
 echo "\t \t \"browser\": \"{$browser}\" \n";
 echo "\t }, \n";
 echo "\t \"extracted\": { \n";
-echo "\t \t \"zone\": \"{$zone}\" \n";
-echo "\t \t \"country\": \"{$country}\" \n";
-echo "\t \t \"region\": \"{$region}\" \n";
-echo "\t \t \"city\": \"{$city}\" \n";
-echo "\t \t \"zip\": \"{$zip}\" \n";
-echo "\t \t \"latitude\": \"{$latitude}\" \n";
-echo "\t \t \"longitude\": \"{$longitude}\" \n";
-echo "\t \t \"cidr\": \"{$cidr}\" \n";
-echo "\t \t \"asn\": \"{$asn}\" \n";
-echo "\t \t \"isp\": \"{$isp}\" \n";
+echo "\t \t \"zone\": \"{$ip2l[0][9]}\" \n";
+echo "\t \t \"country\": \"{$ip2l[0][2]}\" \n";
+echo "\t \t \"region\": \"{$ip2l[0][4]}\" \n";
+echo "\t \t \"city\": \"{$ip2l[0][5]}\" \n";
+echo "\t \t \"zip\": \"{$ip2l[0][8]}\" \n";
+echo "\t \t \"latitude\": \"{$ip2l[0][6]}\" \n";
+echo "\t \t \"longitude\": \"{$ip2l[0][7]}\" \n";
+echo "\t \t \"cidr\": \"{$ip2l[1][2]}\" \n";
+echo "\t \t \"asn\": \"{$ip2l[1][3]}\" \n";
+echo "\t \t \"isp\": \"{$ip2l[1][4]}\" \n";
 echo "\t } \n";
 echo "} \n";
