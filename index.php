fd6f729b4f7169c604bb6a19b0e028d328f53e18
albie6544 <49698766+albie6544@users.noreply.github.com>
2022-04-20 07:45:18 -0400

    Quick Fixes

--- index.php
+++ index.php
@@ -261,7 +261,7 @@ if ($browser == "unknown" || $_GET["api"] == "true") {
 							
 					var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 						maxZoom: 18,
-						attribution: 'Map Data & Imagery &copy; <a href='https://www.openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> & <a href='https://www.mapbox.com/' target='_blank'>Mapbox</a>',
+						attribution: 'Map Data & Imagery &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> & <a href='https://www.mapbox.com/'>Mapbox</a>',
 						id: 'mapbox/streets-v11',
 						tileSize: 512,
 						zoomOffset: -1,
